8a42340713767b2434063244f1dbe5a72deee2bc
Kaelen Juntilla <Da4kast@charter.net>
2015-02-04 15:02:13 -0800

    4th push

--- controller/create-db.php
+++ controller/create-db.php
@@ -10,8 +10,25 @@
     $exists = $connection->select_db($database);
     
     if(!$exists) {
-        echo "Database does not exists";
+        $query = $connection->query("CREATE DATABASE $database");
+        if($query) {
+            echo "Successfully created database:"   . $database;
+        }
     }
+    else {
+        echo "Database already exists";
+    }
+    
+    $query = $connection->query("CREATE TABLE posts ("
+            . "id int (11) NOT NULL AUTO_INCREMENT,"
+            . "title varchar(255) NOT NULL,"
+            . "post text NOT NULL,"
+            . "PRIMARY KEY (id))");
+    
+    if($query) {
+        echo "Succesfully created table: posts";
+    }
+    
   
     
     $connection->close();
