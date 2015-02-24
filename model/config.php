0377115285494092eade3dca91f4673024468923
Kaelen Juntilla <Da4kast@charter.net>
2015-02-24 15:00:56 -0800

    17th push

--- model/config.php
+++ model/config.php
@@ -1,6 +1,8 @@
 <?php
     require_once(__DIR__ . "/database.php");
     session_start();
+    //when the session is started an id is generated and used when the session is active but when the session is restarted another id is generated. this "session_regenerate stops hackers from hacking the blog//
+    session_regenerate_id(true);
     
     $path = "/JuntillaK-Blog/";
     
@@ -8,7 +10,7 @@
     $username = "root";
     $password = "root";
     $database ="blog_db";
-    
+    //The session or this line of code lets the connection variable be used throughout the code//
     if(!isset($_SESSION["connection"])) {
         $connection = new Database($host, $username, $password, $database);
         $_SESSION["connection"] = $connection;
